6b7a43194b3a5602d685dac2d51b83cea85b7928
girda <artem.girda@gmail.com>
2020-02-23 20:46:24 +0200

    create project

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import animation from './components/animation.js';
+import slick from 'slick-carousel';
+
+animation();
+
+$('.slider').slick({
+  dots: true
+});
---
+++ src/js/components/animation.js
@@ -0,0 +1,11 @@
+import { TimelineMax } from 'gsap/all';
+
+export default function animation() {
+  let tl = new TimelineMax();
+
+  tl.fromTo('.js-header-animate', 2, {y: -300, opacity: 0}, {y: 0, opacity: 1});
+  tl.fromTo('.js-hero-animate', 2, {opacity: 0}, {opacity: 1}, 0);
+  tl.fromTo('.js-hero-img-animate', 2, {x: -4000, opacity: 0}, {x: 0, opacity: 1}, 0);
+  tl.fromTo('.js-products-animate', 2, {y: 2000, opacity: 0}, {y: 0, opacity: 1}, 0);
+  // tl.fromTo('.js-preview-animate', 2, {opacity: 0, scale: 0}, {opacity: 1, scale: 1}, 0);
+}
